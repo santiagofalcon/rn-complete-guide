005cf7aa28eca7ce4b0a067f14215b1d814048a2
santiagofalcon <santiagofalcon12@hotmail.com>
2022-02-07 02:20:26 +0100

    ListView

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+ export default function App() {
+     const [enteredGoal, setEnteredGoal] = useState('');
+     const [courseGoals, setCourseGoals] = useState([]);
 
-export default function App() {
+
+     const addGoalHandler = goalTitle =>{
+       setCourseGoals(currentGoals =>[...currentGoals,
+           {id: Math.random().toString(),value: goalTitle }]);
+     };
+
+     const removeGoalHandler = goalId => {
+         setCourseGoals(currentGoals => {
+             return currentGoals.filter((goal) => goal.id !== goalId );
+         });
+     };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+       <GoalInput onAddGoal={addGoalHandler}/>
+        <FlatList
+            keyExtractor={(item, index) => item.id}
+            data={courseGoals}
+                  renderItem={itemData =>
+                      <GoalItem
+                          id={itemData.item.id}
+                          onDelete ={removeGoalHandler}
+                          title = {itemData.item.value}/>
+                  } >
+
+        </FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 50
+    }
 });
